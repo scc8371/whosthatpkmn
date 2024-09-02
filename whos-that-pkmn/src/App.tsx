a1249e55c0c02a27095c080ab9ec5c87a06c706e
Sami Chamberlain <70332182+scc8371@users.noreply.github.com>
2024-09-02 02:18:00 -0400

    comp error fixes

--- whos-that-pkmn/src/App.tsx
+++ whos-that-pkmn/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useTransition } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css'
 import Menu from "./components/Menu"
